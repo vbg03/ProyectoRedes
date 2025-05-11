808ad4252674b9553006c7b29cab551378999182
juanpabloh18 <juan_pablo.hurtado@uao.edu.co>
2025-05-11 18:24:30 -0500

    change listen port and console log

--- Backend/Usuarios/src/index.js
+++ Backend/Usuarios/src/index.js
@@ -6,6 +6,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(usuariosController);
 
-app.listen(3001, () => {
- console.log('backProductos ejecutandose en el puerto 3005');
+app.listen(3005, () => {
+ console.log('Usuarios ejecutandose en el puerto 3005');
 });
